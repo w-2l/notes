3e170807616ffc190e80d13af98a4ab3ae5c48ce
w-2l <wy_w2l@163.com>
2022-11-10 21:33:26 +0800

    Changes

--- c_notes/cLion/main.c
+++ c_notes/cLion/main.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
+#include"test.h"
 
 int main()
 {
-
+    test01();
+    return 0;
 }
---
+++ c_notes/cLion/test.h
@@ -0,0 +1,11 @@
+//
+// Created by DELL on 2022/11/10.
+//
+
+#ifndef CLION_TEST_H
+#define CLION_TEST_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#endif //CLION_TEST_H
