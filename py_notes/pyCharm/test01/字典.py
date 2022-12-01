809c30d27ef0adbe533fc6559faa9474aa72829e
w-2l <wy_w2l@163.com>
2022-12-01 21:30:36 +0800

    add "py_notes "

---
+++ py_notes/pyCharm/test01/字典.py
@@ -0,0 +1,25 @@
+# phone = {'A': '97', 'B': '98', 'C': '99'}
+# print(phone)
+# print(f'len(phone)={len(phone)}')
+# print(f'phone["A"]={phone["A"]}')
+# del phone['B']
+# print(f'phone={phone}')
+# print(f'"B" in d = {("C" in phone)}')
+
+# d = {}
+# d['names'] = ['A', 'B']
+# c = d.copy()
+# print(f'c={c}')
+# from copy import deepcopy
+#
+# d = {}
+# d['names'] = ['A', 'B']
+# c = d.copy()
+# dc = deepcopy(d)
+# d['names'].append('C')
+# print(f'c={c}')
+# print(f'dc={dc}')
+# d = {'a':3}
+# print(d.get('name'))
+# print(d.get('name'), "\b"*5)
+# print(d.get('a',1))
--- py_notes/pyCharm/test01/字符串.py
+++ py_notes/pyCharm/test01/字符串.py
@@ -11,9 +11,36 @@
 # import math
 # tmpl = 'The {mod.__name__} module defiens the value {mod.pi} for PI'
 # print(tmpl.format(mod=math))
+import string
 
 # print("{pi!s} {pi!r} {pi!a}".format(pi='\u03c0'))
 # print('{num:#g} '.format(num=123))
 # print('{num:g} '.format(num=123))
 # print('{num:#b} '.format(num=123))
 # print('{num:b} '.format(num=123))
+# print("Hello world!".center(40))
+#
+# s = "Hello world!"
+# print(s.find('world'))
+# print(s[s.find('o'):])
+# print(s.find('ll', 1, 10))  # 起点和终点
+#
+# seq = ['1', '2', '3', '4']
+# sep = '+'
+# print(sep.join(seq))
+# print(seq)
+#
+# print("hello world".title())
+# print("NAME IS ".lower())
+s = "Hello abc!"
+print(repr(s.replace("abc", "world")))
+print(repr(s))
+
+print('1+2+3+4'.split('+'))
+
+table = str.maketrans('cs', 'kz')  # maketrans方法可以加第三个参数，指定要删除的字母
+print(type(table))
+print(table)
+s = 'this is an incredible test.'
+print(s)
+print(s.translate(table))
