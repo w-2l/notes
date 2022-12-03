8537797fb58d30eef43ce4365173234ac88699e1
w-2l <wy_w2l@163.com>
2022-12-03 21:46:51 +0800

    add c_notes  and vscode

---
+++ c_notes/excise_c/test.c
@@ -0,0 +1,5 @@
+//
+// Created by DELL on 2022/12/3.
+//
+
+#include "test.h"
---
+++ c_notes/excise_c/test.h
@@ -0,0 +1,8 @@
+//
+// Created by DELL on 2022/12/3.
+//
+
+#ifndef EXCISE_C_TEST_H
+#define EXCISE_C_TEST_H
+
+#endif //EXCISE_C_TEST_H
